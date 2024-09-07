c496030d6a826bd45d27748e823f885551e08de6
André Freitas <freitasandre38@gmail.com>
2024-09-07 11:13:09 -0300

    feat: implements obj speed tracker

--- yolo_test.py
+++ yolo_test.py
@@ -1,10 +1,11 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, solutions
 import cv2
 
 
 # Setup
 model = YOLO('yolov8n.pt')
-video_path = 'marathon.mp4'  
+names = model.model.names
+video_path = 'sprint.mp4'  
 cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
@@ -17,18 +18,29 @@ fps = int(cap.get(cv2.CAP_PROP_FPS))
 
 out = cv2.VideoWriter('marathon_output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
 
+line_pts = [(0, 360), (1280, 360)]
+speed_obj = solutions.SpeedEstimator(
+    reg_pts=line_pts,
+    names=names,
+    view_img=True,
+)
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+    
+    tracks = model.track(frame, persist=True, show=False)
+    frame = speed_obj.estimate_speed(frame, tracks)
+    out.write(frame)
+    # cv2.imshow("Marathon Runners Detection", annotated_frame)
 
-    results = model(frame, classes=[0], conf=0.4)
+    # results = model(frame, classes=[0], conf=0.4)
 
-    annotated_frame = results[0].plot() 
+    # annotated_frame = results[0].plot() 
 
-    cv2.imshow("Marathon Runners Detection", annotated_frame)
 
-    out.write(annotated_frame)
+    # out.write(annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
